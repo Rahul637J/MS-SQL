84aa0cb737f27739555da7b23dabb4f387a1437b
Your Name <you@example.com>
2024-09-18 15:20:53 +0530

    [Rahul] Added :Implementing crud operation in SQL server

--- Crud_Operation_in_ssms.sql
+++ Crud_Operation_in_ssms.sql
@@ -1,10 +1,13 @@
 
-
-
+-- Creating database
 create database library_management_system;
 
+
+-- Connecting to database
 use library_management_system;
 
+
+-- Creating table
 create table books(
 	ID int not null,
 	Book_Name varchar(255) ,
@@ -12,12 +15,18 @@ create table books(
 	Genre varchar(255)
 );
 
+
+--Altering table
 alter table books
 alter column ID int not null;
 
 alter table books
 add constraint pk_Books_ID primary key(ID);
 
+EXEC sp_rename 'books.Id', 'Book_Id', 'COLUMN';
+
+
+--Inserting into table
 insert into books(ID,Book_Name,Author,Genre)
 values
 (1,'The Great Gatsby','F. Scott Fitzgerald','classic'),
@@ -31,11 +40,15 @@ values
 (9, 'The Da Vinci Code', 'Dan Brown', 'Thriller'),
 (10, 'Brave New World', 'Aldous Huxley', 'Dystopian');
 
+
+--Read from the table
 select * from library_management_system.dbo.books;
 
+--Updating record in the table
 update books
 set Book_Name = 'To Kill a Mockingbird',Author = 'Harper Lee',Genre = 'classic'
 where ID=3;
+
 
 select * from books;
 
@@ -43,3 +56,54 @@ delete from books
 where ID=9;
 
 select * from books;
+
+
+--Creating new table
+create table student(
+	Student_ID int not null primary key,
+	Student_Name varchar(255) ,
+	Dept varchar(255),
+	Book_Id int
+);
+
+insert into student(Student_ID,Student_Name,Dept,Book_Id)
+values
+(311,'Rahul','MCT',1),
+(312,'Vijay','MCA',2),
+(313,'Venkatesh','MCA',3),
+(314,'Suresh','MECH',4),
+(315,'Naveen','ECE',5),
+(316,'Girish','EEE',6),
+(317,'Sanjay','EEE',7);
+
+Select * from student;
+
+--Fetching from the 2 tables
+
+SELECT student.Student_Name, books.Genre
+FROM student
+JOIN books ON student.Book_Id = books.Book_Id
+where Dept = 'MCA';
+
+--Updating in the book
+UPDATE books
+SET Genre = 'Romance'
+FROM books
+JOIN student ON books.Book_Id = student.Book_Id
+WHERE student.Book_Id = 7;
+
+select * from books;
+
+delete 
+from books where Book_id = 8;
+
+UPDATE student
+SET Book_Id = 5
+where Student_ID = 311;
+
+select * from student;
+
+-- Deleting in student
+delete 
+from student
+where Student_ID = 316;
